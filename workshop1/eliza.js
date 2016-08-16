369675dcbf990235827e9e96931ff313088438de
ph3rb <pb2258@bard.edu>
2016-08-16 12:18:56 -0400

    Update eliza.js

--- workshop1/eliza.js
+++ workshop1/eliza.js
@@ -40,8 +40,8 @@ var rules = {
     "What do you think about computers?",
     "What do you think machines have to do with your problem?",
   ],
-  "~*x name ~*y": [
-    "I am not interested in names",
+  "~*x marco ~*y": [
+    "polo",
   ],
   "~*x sorry ~*y": [
     "Please don't apologize",
